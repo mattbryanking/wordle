e95100b1370573ff7e7b204dc4ef8e842fb8a8ee
Matthew King <matthewkingstudent@gmail.com>
2023-07-20 19:38:23 -0400

    nav bar framework completed

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Board from "./Components/Board/Board";
 import Keyboard from "./Components/Keyboard/Keyboard";
 
 function App() {
-    return <div className="App">
-        <Navbar />
-        <Board />
-        <Keyboard />
-    </div>;
+    return (
+        <div className="App">
+            <Navbar />
+            <Board />
+            <Keyboard />
+        </div>
+    );
 }
 
 export default App;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { ReactComponent as Help } from "./Assets/help.svg";
+import { ReactComponent as Stats } from "./Assets/stats.svg";
+import { ReactComponent as Settings } from "./Assets/settings.svg";
 import "./Navbar.css";
 
 export default function Navbar() {
     return (
         <div className="nav-bar">
-            <h1><span>W</span>ordle</h1>
+            <div className="nav-bar-hamburger">
+                <div />
+                <div />
+                <div />
+            </div>
+            <h1>
+                <span>W</span>ordle
+            </h1>
+            <div className="nav-bar-right">
+                <Help className="nav-bar-help" />
+                <Stats className="nav-bar-stats" />
+                <Settings className="nav-bar-settings" />
+                <button className="nav-bar-subscribe">
+                    Subscribe to Games
+                </button>
+            </div>
         </div>
     );
 }
